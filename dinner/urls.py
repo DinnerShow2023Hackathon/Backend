85b8edcb752e51c052d2f0ce695e9ad527296647
unknown <melgo129@gmail.com>
2023-04-30 01:57:21 +0900

    add serializers.py

--- dinner/urls.py
+++ dinner/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('user/<str:nickname>', views.signup),
+    path('add/<str:nickname>',views.addBook),
+    path('mybooks/<str:nickname>', views.getMyBooks),
+    path('books', views.getAllBooks),
 ]
